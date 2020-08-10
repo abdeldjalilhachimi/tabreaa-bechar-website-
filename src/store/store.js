980097fe1df69a352a1b1c7f7bb5aef730525bd5
“Haabdeldjalil <abdeldjalilhachimi12@gmail.com>
2020-08-10 12:19:29 +0100

    up

--- src/store/store.js
+++ src/store/store.js
@@ -3,17 +3,21 @@ import thunk from "redux-thunk";
 import rootReducer from "./reducers/rootReducer";
 
 const insialState = {};
-/* const composeEnhancers =
-  window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose; */
+const composeEnhancers =
+  window.__REDUX_DEVTOOLS_EXTENSION__COMPOSE__ || compose;
 
 const store = createStore(
   rootReducer,
   insialState,
-  compose(
+  composeEnhancers(applyMiddleware(thunk))
+
+  //composeEnhancers(applyMiddleware(thunk))
+);
+/* 
+
+ compose(
     applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
   )
-  //composeEnhancers(applyMiddleware(thunk))
-);
-
+*/
 export default store;
